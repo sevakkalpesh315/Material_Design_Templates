b11359c08b60c517b38d9b346d96af5cb79c5493
Kalpesh <kalpesh@Kalpeshs-MacBook-Pro-2.local>
2015-02-18 11:30:17 +0000

    Initial commit

---
+++ app/src/main/java/nd/sevakkalpesh/app/navigationdrawer_types/NavigationDrawerCallbacks.java
@@ -0,0 +1,8 @@
+package nd.sevakkalpesh.app.navigationdrawer_types;
+
+/**
+ * Created by poliveira on 27/10/2014.
+ */
+public interface NavigationDrawerCallbacks {
+    void onNavigationDrawerItemSelected(int position);
+}
